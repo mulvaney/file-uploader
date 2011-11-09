e3281005f9717042be8bc29a1f75b79d9548ec07
Mike Mulvaney <mike.mulvaney@gmail.com>
2011-11-09 22:49:39 +0800

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FileUploader::Application.config.secret_token = '393a967d0edc8c82068e089de8b69f1af2d81d60519f93fc5a31b15039998214e2145d2cb918cadf91248d387e146f98b6061d7adf5b8b46a4014cb3b5ebcb36'
