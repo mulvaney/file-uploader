2fc43bbb8002be682efbe04478a8a1791baac670
Mike Mulvaney <mike.mulvaney@gmail.com>
2011-11-10 01:06:53 +0800

    tests working for devise stuff

--- test/functional/documents_controller_test.rb
+++ test/functional/documents_controller_test.rb
@@ -1,49 +1,79 @@
 require 'test_helper'
 
+
 class DocumentsControllerTest < ActionController::TestCase
-  setup do
-    @document = documents(:one)
+  include Devise::TestHelpers
+
+  context "Without a user" do
+    should "redirect to sign in" do
+      get :index
+      assert_redirected_to user_session_path
+    end
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:documents)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create document" do
-    assert_difference('Document.count') do
-      post :create, document: @document.attributes
+  context "with an unconfirmed user" do
+    should "redirect to sign in" do
+      get :index
+      assert_response 302
     end
 
-    assert_redirected_to document_path(assigns(:document))
-  end
-
-  test "should show document" do
-    get :show, id: @document.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @document.to_param
-    assert_response :success
-  end
-
-  test "should update document" do
-    put :update, id: @document.to_param, document: @document.attributes
-    assert_redirected_to document_path(assigns(:document))
-  end
-
-  test "should destroy document" do
-    assert_difference('Document.count', -1) do
-      delete :destroy, id: @document.to_param
+    should "redirect with out creating document" do
+      assert_no_difference('Document.count') do
+        @document = documents(:one)
+        post :create, document: @document.attributes
+      end
+      
+      assert_redirected_to user_session_path
     end
 
-    assert_redirected_to documents_path
+  end
+
+  context "with a signed in user" do
+    setup do
+      sign_in users(:testuser)
+      @document = documents(:one)
+    end
+
+    should "get index" do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:documents)
+    end
+
+    should "get new" do
+      get :new
+      assert_response :success
+    end
+
+    should "create document" do
+      assert_difference('Document.count') do
+        post :create, document: @document.attributes
+      end
+      
+      assert_redirected_to document_path(assigns(:document))
+    end
+
+    should " show document" do
+      get :show, id: @document.to_param
+      assert_response :success
+    end
+
+    should " get edit" do
+      get :edit, id: @document.to_param
+      assert_response :success
+    end
+
+    should " update document" do
+      put :update, id: @document.to_param, document: @document.attributes
+      assert_redirected_to document_path(assigns(:document))
+    end
+
+    should "destroy document" do
+      assert_difference('Document.count', -1) do
+        delete :destroy, id: @document.to_param
+      end
+
+      assert_redirected_to documents_path
+    end
   end
 end
