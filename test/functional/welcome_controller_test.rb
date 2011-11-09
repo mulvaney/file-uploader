b7d8a91a12ec134610fca45e13004a674fa39075
Mike Mulvaney <mike.mulvaney@gmail.com>
2011-11-10 01:24:48 +0800

    more testing, making login the default thing

--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -1,9 +1,35 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
+  include Devise::TestHelpers
+
+  context "Without a user" do
+    should "redirect to sign in" do
+      get :index
+      assert_redirected_to user_session_path
+    end
+  end
+
+  context "with an unconfirmed user" do
+    setup do
+      sign_in users(:unconfirmed)
+    end
+
+    should "redirect to sign in" do
+      get :index
+      assert_redirected_to user_session_path
+    end
+  end
+
+  context "with a logged in user" do
+    setup do
+      sign_in users(:testuser)
+    end
+
+    should "show index" do
+      get :index
+      assert_response :success
+    end
   end
 
 end
